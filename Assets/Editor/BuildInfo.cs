65bbafbd080bd76875288300e36017ae3f31ba37
Cass <cass@supertree.co>
2023-12-13 18:10:58 +0900

    *Update

--- Assets/Editor/BuildInfo.cs
+++ Assets/Editor/BuildInfo.cs
@@ -4,7 +4,7 @@ public class BuildInfo
 {
 	public const BuildTarget buildTarget = BuildTarget.WebGL;
 	public const Builder.ReleaseType releaseType = Builder.ReleaseType.Dev;
-	public const string BundleVersion = "2.0.10";
-	public const string BuildDate = "2023-12-13 17:58:13";
+	public const string BundleVersion = "2.0.11";
+	public const string BuildDate = "2023-12-13 18:09:03";
 }
 
