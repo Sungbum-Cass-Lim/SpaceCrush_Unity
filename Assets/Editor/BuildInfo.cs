c4db570df2cf1ea6f1afbac0a6440b2e87986659
Cass <cass@supertree.co>
2023-12-26 10:24:41 +0900

    *Templete DevicePixelRatio Setting

--- Assets/Editor/BuildInfo.cs
+++ Assets/Editor/BuildInfo.cs
@@ -3,8 +3,8 @@ using UnityEditor;
 public class BuildInfo
 {
 	public const BuildTarget buildTarget = BuildTarget.WebGL;
-	public const Builder.ReleaseType releaseType = Builder.ReleaseType.Dev;
-	public const string BundleVersion = "2.0.38";
-	public const string BuildDate = "2023-12-15 16:27:31";
+	public const Builder.ReleaseType releaseType = Builder.ReleaseType.LocalCass;
+	public const string BundleVersion = "2.0.39";
+	public const string BuildDate = "2023-12-26 10:13:49";
 }
 
