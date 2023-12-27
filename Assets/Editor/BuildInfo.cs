14c0f4864fcd01eac402c531c7a3dd636bc70c84
Cass <cass@supertree.co>
2023-12-27 17:49:19 +0900

    *WebLoding Align center

--- Assets/Editor/BuildInfo.cs
+++ Assets/Editor/BuildInfo.cs
@@ -3,8 +3,8 @@ using UnityEditor;
 public class BuildInfo
 {
 	public const BuildTarget buildTarget = BuildTarget.WebGL;
-	public const Builder.ReleaseType releaseType = Builder.ReleaseType.Qa;
-	public const string BundleVersion = "2.0.42";
-	public const string BuildDate = "2023-12-26 15:21:06";
+	public const Builder.ReleaseType releaseType = Builder.ReleaseType.LocalCass;
+	public const string BundleVersion = "2.0.43";
+	public const string BuildDate = "2023-12-27 16:05:03";
 }
 
